213435520675df2cff9ee8d9d21ed5fd390baf80
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-10 13:55:42 +0100

    File to wipe database - except users

---
+++ prisma/refreshDatabase.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.messageReciept.deleteMany({})
+    await prisma.message.deleteMany({})
+    await prisma.group.deleteMany({})
+    await prisma.directMessage.deleteMany({})
+    console.log('Fake data deleted successfully');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+
