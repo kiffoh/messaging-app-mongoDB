56b3e094d2d8342b2031ad671c2fed76ae3c2471
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-15 16:05:24 +0100

    Added routes for creating new groups

--- routes/groups.js
+++ routes/groups.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const groupController = require('../controllers/groupController')
+const upload = require('../configuration/multerConfig')
 
 router.get('/:groupId/profile', groupController.getGroup);
+
+router.post('/createDirectMessage', groupController.createDirectMessage);
+
+router.post('/createGroup', upload.single('groupPhoto'), groupController.createGroup);
 
 module.exports = router;
