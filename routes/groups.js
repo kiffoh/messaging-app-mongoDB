035d4a1c5b7d61ea57dc342cbe3ca01b61649b0d
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-21 13:20:50 +0100

    Add deleteGroup route

--- routes/groups.js
+++ routes/groups.js
@@ -7,6 +7,8 @@ router.get('/:groupId/profile', groupController.getGroup);
 
 router.put('/:groupId/profile', groupController.updateGroup);
 
+router.delete('/:groupId/profile', groupController.deleteGroup);
+
 router.post('/createDirectMessage', groupController.createDirectMessage);
 
 router.post('/createGroup', upload.single('groupPhoto'), groupController.createGroup);
