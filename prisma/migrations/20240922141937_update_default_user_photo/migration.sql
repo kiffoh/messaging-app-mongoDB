ff45de5aace4f3ebc3f88e6df3b34e86c78df217
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-23 21:11:11 +0100

    Update default photo

---
+++ prisma/migrations/20240922141937_update_default_user_photo/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "photo" SET DEFAULT 'https://res.cloudinary.com/dmaq0peyx/image/upload/v1727014580/1_person_high_resolution_pyzwja.jpg';
---
+++ prisma/migrations/20240922173546_update_default_user_photo/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "photo" SET DEFAULT 'https://res.cloudinary.com/dmaq0peyx/image/upload/v1725813381/o3aadn8b9aww4wuzethd.svg';
