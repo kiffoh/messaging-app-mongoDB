15543a0908af1f973e14be36cc93c982d2446458
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-10-15 16:27:22 +0100

    validation for update of group

--- routes/groups.js
+++ routes/groups.js
@@ -1,16 +1,41 @@
 var express = require('express');
 var router = express.Router();
 const groupController = require('../controllers/groupController')
-const { uploadGroupPhoto } = require('../configuration/multerConfig')
+const { uploadGroupPhoto } = require('../configuration/multerConfig');
+const { validateGroup } = require('../configuration/validation'); // Import the validation middleware
+const { validationResult } = require('express-validator');
 
 router.get('/:groupId/profile', groupController.getGroup);
 
-router.put('/:groupId/profile', uploadGroupPhoto.single('photo'), groupController.updateGroup);
+router.put('/:groupId/profile', 
+    uploadGroupPhoto.single('photo'), 
+    validateGroup, // Add validation middleware
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors.array())
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    },
+    groupController.updateGroup
+);
 
 router.delete('/:groupId/profile', groupController.deleteGroup);
 
 router.post('/createDirectMessage', groupController.createDirectMessage);
 
-router.post('/createGroup', uploadGroupPhoto.single('groupPhoto'), groupController.createGroup);
+router.post('/createGroup', 
+    uploadGroupPhoto.single('groupPhoto'), 
+    validateGroup,
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()})
+        }
+        next();
+    },
+    groupController.createGroup
+);
 
 module.exports = router;
