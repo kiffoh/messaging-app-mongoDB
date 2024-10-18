58f64123f2a7c2e15b0838540dc6644572cf7004
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-10-18 17:02:49 +0100

    Update socket.io origin address to match frontend

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var server = http.createServer(app);
 // Initialize socket.io server
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://messaging-app-client-eight.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
