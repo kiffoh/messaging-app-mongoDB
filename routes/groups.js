130358c290a6030a83a82c053ba9dcf3575db8e8
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-10-20 16:27:41 +0100

    Console.logs for debugging

--- routes/groups.js
+++ routes/groups.js
@@ -30,6 +30,7 @@ router.post('/createGroup',
     validateGroup,
     (req, res, next) => {
         const errors = validationResult(req);
+        console.log(errors)
         if (!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()})
         }
