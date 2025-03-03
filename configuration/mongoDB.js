e1474c293e36c9abb5223a33278606f3ce391806
Kiff <kiffoh@gmail.com>
2025-03-03 13:52:02 +0000

    Creates and manages connection to database

---
+++ configuration/mongoDB.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+
+const mongo = new MongoClient(process.env.MONGO_DB_CONNECTION_STRING);
+let db;
+
+async function connectDB() {
+    try {
+        await mongo.connect();
+        db = mongo.db('messagingApp');
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
+}
+
+process.on('SIGINT', async () => {
+    await mongo.close();
+    console.log('MongoDB connection closed');
+    process.exit(0);
+  });
+  
+  process.on('SIGTERM', async () => {
+    await mongo.close();
+    console.log('MongoDB connection closed');
+    process.exit(0);
+  });
+  
+  module.exports = { connectDB, db: () => db, client: () => mongo };
